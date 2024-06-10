8710817ab69686645699ea026ac1fd0c17348ad6
Atharv Sathe <98419532+atharv3266@users.noreply.github.com>
2024-06-10 23:11:41 +0530

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+x= "This is my world"
+def lol():
+    y="Welcome"
+    print(locals())
+print(globals())
+lol()
---
+++ script2.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+target = "cdac.in"
+print(target)
+
+x=""" this is sparta,
+HEllo"""
+print(x)
+
+x=x+"1"
+print(x)
